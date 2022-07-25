4c867d3882616b46cbde3b6af9278b534c4239b0
Denis Svetleishii <denissvetleishii@outlook.com>
2022-07-25 23:29:24 +0400

    feat: add js for pop up

--- travel/index.js
+++ travel/index.js
@@ -1,3 +1,5 @@
+
+
 // Burger header
 (function () {
     const burgerItem = document.querySelector('.burger');
@@ -9,4 +11,44 @@
     menuCloseItem.addEventListener('click', () =>{
         menu.classList.remove('header-nav-active');
     })
+
+// pop-up log in
+
+
+    const popupBackgound = document.querySelector('.log-in-pop-up-background');
+    const popUpContent = document.querySelector('.log-in-pop-up');
+    const openPopUp = document.querySelector('.button-login');  
+    openPopUp.addEventListener('click', () =>{
+        popupBackgound.classList.add('log-in-pop-up-background-active');
+        popUpContent.classList.add('log-in-pop-up-active');
+    });
+
+
+
+    popupBackgound.addEventListener('click', () =>{
+        popupBackgound.classList.remove('log-in-pop-up-background-active');
+        popUpContent.classList.remove('log-in-pop-up-active');    
+    }
+        )
+
+// aret email name
+    const welcome = () => {
+        let email = document.querySelector('.email');
+        let password = document.querySelector('.password');
+        if(email.value || password.value){
+            alert(`Ваш email: ${email.value}, Ваш пароль: ${password.value}`);
+        }
+        else{
+            alert('Заполните поля');
+        }
+    }
+    document.querySelector('.sign-in-button').addEventListener('click', welcome)
+
+
+
+
 }())
+
+
+
+
